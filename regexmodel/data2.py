3d18587eef8ca5136f3201447b30fb90b7ee69b6
Raoul Schram <r.d.schram@uu.nl>
2023-09-04 10:30:30 +0200

    Start working on statistics

--- regexmodel/data2.py
+++ regexmodel/data2.py
@@ -1,5 +1,5 @@
 import numpy as np
-from regexmodel.util import Dir
+from regexmodel.util import Dir, sum_prob_log
 from regexmodel.regexclass import BaseRegex
 
 
@@ -25,6 +25,18 @@ class RegexNode(BaseNode):
     def count(self):
         return self.next.count
 
+    @property
+    def n_param(self):
+        return self.next.n_param
+
+    def log_likelihood(self, value):
+        all_probs = []
+        log_prob_next = []
+        for post_str, prob in self._regex.fit_value(value):
+            all_probs.append(prob)
+            log_prob_next.append(self.next.log_likelihood(post_str))
+        return sum_prob_log(all_probs, log_prob_next)
+
 
 class OrNode(BaseNode):
     def __init__(self, edges, next_edge):
@@ -47,18 +59,34 @@ class OrNode(BaseNode):
     def __repr__(self):
         return f"{self.__class__.__name__} <{self.regex}> -> <{self.next.regex}>"
 
+    @property
+    def n_param(self):
+        sub_param = np.sum([edge.n_param for edge in self.edges])
+        return sub_param + self.next.n_param
+
+    def log_likelihood(self, value):
+        probs = np.array([edge.count for edge in self.edges])
+        probs /= np.sum(probs)
+        log_likes = np.array([edge.log_like(value) for edge in self.edges])
+        return sum_prob_log(probs, log_likes)
+
 
 class Edge():
-    def __init__(self, destination, count=None, direction=Dir.RIGHT):
+    def __init__(self, destination, count=None):
         if count is None:
             assert destination is not None
             count = destination.count
         self.count = count
         self.destination = destination
-        self.direction = direction
 
     @property
     def regex(self):
         if self.destination is None:
             return ""
         return self.destination.full_regex
+
+    @property
+    def n_param(self):
+        if self.destination is None:
+            return 1
+        return self.destination.n_param + 1
