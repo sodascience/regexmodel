d15e314417d3c36cc5d666985d84699deb513d81
qubixes <44498096+qubixes@users.noreply.github.com>
2023-09-14 10:01:20 +0200

    Add not-fitted error and fix count_thres issues (#3)

--- regexmodel/__init__.py
+++ regexmodel/__init__.py
@@ -1,5 +1,6 @@
 """Regex model for fitting and generating structured strings."""
 
 from regexmodel.model import RegexModel
+from regexmodel.util import NotFittedError
 
-__all__ = ["RegexModel"]
+__all__ = ["RegexModel", "NotFittedError"]
